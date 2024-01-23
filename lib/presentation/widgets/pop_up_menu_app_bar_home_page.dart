606fbc2ef9bceb16d83b0e388b171d4aacc1693a
BolisettySujith <bsujith116@gmail.com>
2024-01-23 11:24:02 +0530

    Adds pop up menu in the app bar

---
+++ lib/presentation/widgets/pop_up_menu_app_bar_home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shutter_stocks_task/logic/blocs/shutterstock_bloc/shutterstock_bloc.dart';
+import 'package:shutter_stocks_task/logic/blocs/shutterstock_bloc/shutterstock_event.dart';
+import 'package:shutter_stocks_task/logic/blocs/shutterstock_bloc/shutterstock_state.dart';
+import 'package:shutter_stocks_task/res/app_colors.dart';
+import 'package:shutter_stocks_task/res/app_constants.dart';
+
+class PopUpMenuAppBarHomePage extends StatefulWidget {
+  const PopUpMenuAppBarHomePage({super.key});
+
+  @override
+  State<PopUpMenuAppBarHomePage> createState() => _PopUpMenuAppBarHomePageState();
+}
+
+class _PopUpMenuAppBarHomePageState extends State<PopUpMenuAppBarHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ShutterStockBloc, ShutterStockState>(
+        builder: (context, shutterState){
+          if(shutterState is ShutterStockImagesLoaded) {
+            return PopupMenuButton<int>(
+              itemBuilder: (BuildContext context) => AppConstants.assetTypes.asMap().entries.map((entry) {
+                int index = entry.key;
+                String type = entry.value;
+                return PopupMenuItem(
+                  value: index,
+                  labelTextStyle: MaterialStateTextStyle.resolveWith((states) {
+                    return TextStyle(
+                        fontWeight: shutterState.imgAssetType == ImgAssetTypes.values[index] ? FontWeight.bold : FontWeight.normal,
+                        color: AppColors.popUpMenuLabelTextColor
+                    );
+                  }),
+                  onTap: () {
+                    BlocProvider.of<ShutterStockBloc>(context).add(ChangeAssetTypeEvent(ImgAssetTypes.values[index]));
+                  },
+                  child: Wrap(
+                    children: [
+                      Text(type),
+                      const SizedBox(width: 2,),
+                      shutterState.imgAssetType == ImgAssetTypes.values[index]
+                        ? const Icon(
+                          Icons.check,
+                          size: 10,
+                          color: AppColors.checkIconPopUpMenuItemColor
+                        )
+                        : const SizedBox()
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          }
+          return Container();
+        }
+    );
+  }
+}
